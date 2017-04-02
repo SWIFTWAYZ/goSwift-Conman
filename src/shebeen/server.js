95e65e18ffa0d52e2b6b939dcb4a2cab9366a0d6
dash <dashville@ymail.com>
2017-04-02 22:17:24 +0200

    added server.js to conman

---
+++ src/shebeen/server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dashville on 2017/04/02.
+ */
+var blueBird = require('bluebird');
+
+var promisedFunc = blueBird.promisify(function(){
+    console.log('promise');
+});
+
+promisedFunc();
+
+ console.log('server running');
